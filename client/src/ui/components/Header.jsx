0f793164dc9e001336c24176e30c724e5450aeb0
joebazzzz <joe0bazzzz@gmail.com>
2023-08-11 19:40:11 +0100

    Basic page created

---
+++ client/src/ui/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return <>
+    <div className="Header-container">
+        <div className="Header-title">Forisbeeum</div>
+        <div className="Header-link">Home</div>
+        <div className = "Header-link">Profile</div>
+        <div className = "Header-link">Settings</div>
+    </div>
+    </>
+}
+
+
+export default Header
---
+++ client/src/ui/components/Popup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Popup = () => {
+    return <div className = "Popup">POPUP</div>
+}
+
+export default Popup
