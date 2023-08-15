a2d28744189e58a30b456b15e8a8a1b6a813b21f
joebazzzz <joe0bazzzz@gmail.com>
2023-08-15 22:03:13 +0100

    new file:   client/src/ui/Pages/Profile.jsx 	modified:   client/src/App.css 	modified:   client/src/App.js 	modified:   client/src/ui/components/Header.jsx

---
+++ client/src/ui/Pages/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from '../components/Login'
+
+function Profile() {
+    return <>
+        <div className="grid">
+            <br></br>
+            <Login/>
+            
+        </div>
+    </>
+};
+
+export default Profile;
+
+
